c2fdc0f54c0c75be91c990b29e0d0a99732ed30c
Dmitry Lupar <d.lupar@dixy.ru>
2025-06-27 00:02:46 +0300

    Задание 1, 2. Начало задания 3

---
+++ src/microservices/events/Consumer.cs
@@ -0,0 +1,53 @@
+﻿using Confluent.Kafka;
+
+namespace Events;
+
+public class Consumer : BackgroundService
+{
+    private readonly ILogger<Consumer> _logger;
+    private readonly ConsumerConfig _consumerConfig;
+
+    public Consumer(ILogger<Consumer> logger)
+    {
+        _logger = logger;
+        _consumerConfig = new ConsumerConfig
+        {
+            BootstrapServers = Environment.GetEnvironmentVariable("KAFKA_BROKERS"),
+            GroupId = "1",
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        await Consume(stoppingToken);
+    }
+
+    public async Task Consume(CancellationToken cancellationToken)
+    {
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            try
+            {
+                using var consumer = new ConsumerBuilder<Ignore, string>(_consumerConfig).Build();
+                consumer.Subscribe(["user-events", "payment-events", "movie-events"]);
+
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    var consumeResult = consumer.Consume(cancellationToken);
+
+                    _logger.LogInformation("Received message from topic '{Topic}':{Message}",
+                        consumeResult.Topic,
+                        consumeResult.Message.Value);
+                }
+
+                consumer.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                await Task.Delay(3000, cancellationToken);
+            }
+        }
+    }
+}
